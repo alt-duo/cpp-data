46e407cabe7f269388c7b90216b601b843a73795
Max <maxlovesairandteslas@gmail.com>
2022-07-30 12:15:49 -0700

    edit

---
+++ homework11/necklace/necklace.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    string s;
+    cin>>n;
+    cin>>s;
+    s = s+s+s;
+    for(int i = 0;i<n;i++){
+        int pos = i+n;
+    }
+}
