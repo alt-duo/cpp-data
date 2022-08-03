38f8d0c42af448bb54db8c7598b0f343b79b2d5e
Max <maxlovesairandteslas@gmail.com>
2022-08-02 22:34:07 -0700

    Update fac.cpp

--- 8-2-2022/fac.cpp
+++ 8-2-2022/fac.cpp
@@ -3,24 +3,25 @@
 using namespace std;
 int main() {
     queue<int> myq; 
-    int total =0;
+    int total =1;
     int n;
     cin>>n;
     int starting = n;
     while(total!=starting){
-        for(int i = 2;i<n;i++){
-            if(n%i==0){
+        for(int i = 2;i<=n;i++){
+            //cout<<" i: "<<i<<" total: "<<total<<endl;
+            while(n%i==0){
                 n=n/i;
                 myq.push(i);
-                total = total + i;
-                break;
+                total = total * i;
+                //cout<<" i: "<<i<<" total: "<<total<<endl;
             }
         }
     }
     while(!myq.empty()){
         cout<<myq.front();
         myq.pop();
-        if(myq.size()!=1){
+        if(myq.size()>0){
             cout<<"*";
         }
     }
