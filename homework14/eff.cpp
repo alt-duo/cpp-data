3a1b562880ae262f1e0a7965c65a985706fd1326
Max <maxlovesairandteslas@gmail.com>
2022-08-05 22:44:28 -0700

    homework2

---
+++ homework14/eff.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    
+    cin>>n;
+    long long total = 1;
+    for(int i = 0;i<n-1;i++){
+        total = 2*total + 2;
+    }
+    cout<<total;
+    
+}
