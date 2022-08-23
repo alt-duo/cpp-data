ec4711a03530ee9999b6025ce21c041ac27ec180
Max <maxlovesairandteslas@gmail.com>
2022-08-22 21:49:20 -0700

    Update selectm.cpp

--- homework35/selectm.cpp
+++ homework35/selectm.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int n, m, counter = 0;
-  cin>>n>>m;
-  int nums[n];
-  for(int i = 0;i<n;i++){
-    cin>>nums[i];
-  }
-  for(int starting = 0; starting<n;starting++){
-    for(int first = starting;first<=n-1;first++){
-      for(int second = first+1;second<=n;second++){
-        if(first+second==m){
-          counter++;
-        }
-      }
-    }
-  }
-  cout<<counter;
+    
+    int n, m, starting = 0, temp, counter = 0;
+    cin>>n>>m;
+    int list1[n];
+    for(int i = 0;i<n;i++){
+        cin>>temp;
+        list1[i] = temp;
 
+    }
+    for(int i = 0;i<(n-1);i++){
+        for(int j = i+1;j<n;j++){
+            if(list1[i]+list1[j]==m){
+                counter++;
+            }
+        }
+    }
+    cout<<counter;
 }
